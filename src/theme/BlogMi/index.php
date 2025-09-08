879a5ac0a32850f7939326de42c3abc1f5314d8e
Jackson Dou <jksdou@qq.com>
2025-09-08 16:10:04 +0800

    update

---
+++ src/theme/BlogMi/index.php
@@ -0,0 +1,131 @@
+<?php
+if (!isset($app_config)) exit;
+
+?>
+<!DOCTYPE html>
+<html lang="zh-CN">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <meta content="telephone=no,email=no" name="format-detection" />
+  <meta name="apple-mobile-web-app-capable" content="yes" />
+  <meta name="apple-mobile-web-app-status-bar-style" content="black" />
+  <meta name="description" content="<?php echo $app_config['site_desc'];?> - DouPress" />
+  <meta name="keywords" content="DouPress" />
+  <meta name="generator" content="DouPress"/>
+  <meta name="author" content="BlogMi" />
+  <link rel="icon" href="<?php app_site_link(); ?>/favicon.ico" />
+  <title><?php if (app_is_post() || app_is_page()) {
+    app_post_title(); ?> | <?php app_site_name();
+  } else {
+    app_site_name(); ?> | <?php app_site_desc();
+  } ?></title>
+  <link href="<?php app_theme_url('style.css'); ?>" type="text/css" rel="stylesheet" />
+</head>
+
+<body>
+  <div id="main">
+    <div id="header">
+      <div id="sitename"><a href="<?php app_site_link(); ?>" title="<?php app_site_desc(); ?>"><?php app_site_name(); ?></a></div>
+    </div>
+    <div class="clear"></div>
+    <div id="sidebar">
+      <div class="photo"><img src="<?php app_theme_url('photo.jpg'); ?>"></div>
+      <div class="about"><?php app_site_desc(); ?></div>
+      <div id="navbar">
+        <a href="<?php app_site_link(); ?>" class="home" title="首页">首页</a>
+        <a href="<?php app_get_url('archive'); ?>" class="archive" title="文章存档">文章存档</a>
+        <a href="<?php app_get_url('about'); ?>" class="contact" title="联系方式">联系方式</a>
+        <a href="<?php app_get_url('rss'); ?>" class="rss" title="RSS订阅" target="_blank">RSS订阅</a>
+      </div>
+      <div class="clear"></div>
+      <div id="footer">Powered by <a href="http://doupress.org" target="_blank">DouPress</a></div>
+    </div>
+    <div id="content">
+      <div id="content_box">
+        <?php if (app_is_post()) { ?>
+          <div class="post">
+            <h1 class="title"><?php app_post_link(); ?></h1>
+            <div class="content">
+              <?php app_post_content(); ?>
+            </div>
+            <div class="post_meta">
+              <div class="post_date"><?php app_post_date(); ?></div>
+              <div class="post_tag"><?php app_post_tags('', '', ''); ?></div>
+              <div class="post_comm"><a href="<?php
+              //app_post_link(); 
+              ?>#comm">评论</a></div>
+            </div>
+          </div>
+        <?php if (app_can_comment()) { ?>
+          <div id="comm"><?php app_comment_code(); ?></div>
+        <?php } ?>
+
+        <?php } else if (app_is_page()) { ?>
+          <div class="post">
+            <h1 class="title"><?php
+             app_post_title();
+            ?></h1>
+            <div class="content">
+              <?php app_post_content(); ?>
+            </div>
+          </div>
+          <?php if (app_can_comment()) { ?>
+            <div id="comm"><?php app_comment_code(); ?></div>
+          <?php } ?>
+        <?php } else if (app_is_archive()) { ?>
+          <div class="post">
+            <h1 class="title">文章存档</h1>
+            <div class="content">
+              <table width="280" border="0" align="center" cellpadding="0" cellspacing="0" style="margin:30px auto;">
+                <tbody>
+                  <tr>
+                    <td width="140" style="vertical-align:top;">
+                      <h1 class="date_list">月份</h1>
+                      <ul id="list"><?php app_date_list(); ?></ul>
+                    </td>
+                    <td width="140" style="vertical-align:top;">
+                      <h1 class="tag_list">标签</h1>
+                      <ul id="list"><?php app_tag_list(); ?></ul>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        <?php } else { ?>
+          <?php if (app_is_tag()) { ?>
+            <div id="page_info"><span><?php app_tag_name(); ?></span></div>
+          <?php } else if (app_is_date()) { ?>
+            <div id="page_info"><span><?php app_date_name(); ?></span></div>
+          <?php } ?>
+          <?php while (app_next_post()) { ?>
+            <div class="post">
+              <h1 class="title"><?php app_post_link(); ?></h1>
+              <div class="content">
+                <?php app_post_content(); ?>
+              </div>
+              <div class="post_meta">
+                <div class="post_date"><?php app_post_date(); ?></div>
+                <div class="post_tag"><?php app_post_tags('', '', ''); ?></div>
+                <div class="post_comm"><a href="<?php 
+                //app_post_link(); 
+                ?>#comm">评论</a></div>
+              </div>
+            </div>
+          <?php } ?>
+      </div>
+      <div class="clear"></div>
+      <div id="page_bar">
+        <?php if (app_has_new()) { ?><?php app_goto_new('«'); ?><?php } ?>
+        <?php if (app_has_old()) { ?><?php app_goto_old('»'); ?><?php } ?>
+      </div>
+    <?php } ?>
+    </div>
+  </div>
+<?php app_footer_code(); ?>
+</body>
+
+</html>
--- src/theme/default/index.php
+++ src/theme/default/index.php
@@ -1,5 +1,5 @@
 <?php
-if (!isset($dp_config)) exit;
+if (!isset($app_config)) exit;
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -11,82 +11,81 @@ if (!isset($dp_config)) exit;
   <meta content="telephone=no,email=no" name="format-detection" />
   <meta name="apple-mobile-web-app-capable" content="yes" />
   <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-  <meta name="description" content="<?php echo $dp_config['site_desc'];?> - DouPress" />
+  <meta name="description" content="<?php echo $app_config['site_desc'];?> - DouPress" />
   <meta name="keywords" content="DouPress" />
   <meta name="generator" content="DouPress"/>
-  <meta name="author" content="上海程江科技中心" />
-  <meta name="copyright" content="程江® 程江科技 CROGRAM" />
-  <link rel="icon" href="<?php dp_site_link(); ?>/favicon.ico" />
-  <title><?php if (dp_is_post() || dp_is_page()) { dp_post_title(); ?> | <?php dp_site_name();} else { dp_site_name(); ?> | <?php dp_site_desc(); } ?></title>
-  <link href="<?php dp_theme_url('style.css'); ?>" type="text/css" rel="stylesheet" />
+  <meta name="author" content="Jackson" />
+  <link rel="icon" href="<?php app_site_link(); ?>/favicon.ico" />
+  <title><?php if (app_is_post() || app_is_page()) { app_post_title(); ?> | <?php app_site_name();} else { app_site_name(); ?> | <?php app_site_desc(); } ?></title>
+  <link href="<?php app_theme_url('style.css'); ?>" type="text/css" rel="stylesheet" />
 </head>
 
 <body>
   <div id="main">
     <div id="header">
       <div id="sitename">
-        <a href="<?php dp_site_link(); ?>" class="link"><?php dp_site_name(); ?></a>
+        <a href="<?php app_site_link(); ?>" class="link"><?php app_site_name(); ?></a>
       </div>
     </div>
     <div id="sidebar">
       <div id="navbar">
         <ul>
-          <li><a href="<?php dp_site_link(); ?>" class="link">首页</a></li>
-          <li><a href="<?php dp_get_url('archive'); ?>" class="link">存档</a></li>
-          <li><a href="<?php dp_get_url('rss'); ?>" class="link">订阅</a></li>
+          <li><a href="<?php app_site_link(); ?>" class="link">首页</a></li>
+          <li><a href="<?php app_get_url('archive'); ?>" class="link">存档</a></li>
+          <li><a href="<?php app_get_url('rss'); ?>" class="link">订阅</a></li>
         </ul>
       </div>
     </div>
     <div id="content">
       <div id="content_box">
-        <?php if (dp_is_post()) { ?>
+        <?php if (app_is_post()) { ?>
           <div class="post">
-            <h1 class="title"><?php dp_post_link(); ?></h1>
-            <div class="tags"><?php dp_post_tags('', '', ''); ?> by <?php dp_nick_name(); ?> at <?php dp_post_date(); ?></div>
-            <div class="content"><?php dp_post_content(); ?></div>
+            <h1 class="title"><?php app_post_link(); ?></h1>
+            <div class="tags"><?php app_post_tags('', '', ''); ?> by <?php app_nick_name(); ?> at <?php app_post_date(); ?></div>
+            <div class="content"><?php app_post_content(); ?></div>
           </div>
-          <?php if (dp_can_comment()) {
-            dp_comment_code();
+          <?php if (app_can_comment()) {
+            app_comment_code();
           } ?>
-        <?php } else if (dp_is_page()) { ?>
+        <?php } else if (app_is_page()) { ?>
           <div class="post">
-            <?php /*<h1 class="title"><?php dp_post_link(); ?></h1>
-            <div class="tags">by <?php dp_nick_name(); ?> at <?php dp_post_date(); ?></div> */ ?>
-            <div class="content"><?php dp_post_content(); ?></div>
+            <?php /*<h1 class="title"><?php app_post_link(); ?></h1>
+            <div class="tags">by <?php app_nick_name(); ?> at <?php app_post_date(); ?></div> */ ?>
+            <div class="content"><?php app_post_content(); ?></div>
           </div>
-          <?php if (dp_can_comment()) { ?>
-            <?php dp_comment_code(); ?>
+          <?php if (app_can_comment()) { ?>
+            <?php app_comment_code(); ?>
           <?php } ?>
-        <?php } else if (dp_is_archive()) { ?>
+        <?php } else if (app_is_archive()) { ?>
           <div class="date_list">
             <h1>月份</h1>
-            <ul><?php dp_date_list(); ?></ul>
+            <ul><?php app_date_list(); ?></ul>
           </div>
           <div class="tag_list">
             <h1>标签</h1>
-            <ul><?php dp_tag_list(); ?></ul>
+            <ul><?php app_tag_list(); ?></ul>
           </div>
           <div class="clearer"></div>
         <?php } else { ?>
-          <?php if (dp_is_tag()) { ?>
-            <div id="page_info">标签：<span><?php dp_tag_name(); ?></span></div>
-          <?php } else if (dp_is_date()) { ?>
-            <div id="page_info">月份：<span><?php dp_date_name(); ?></span></div>
+          <?php if (app_is_tag()) { ?>
+            <div id="page_info">标签：<span><?php app_tag_name(); ?></span></div>
+          <?php } else if (app_is_date()) { ?>
+            <div id="page_info">月份：<span><?php app_date_name(); ?></span></div>
           <?php } ?>
           <div class="post_list">
-            <?php while (dp_next_post()) { ?>
+            <?php while (app_next_post()) { ?>
               <div class="post">
-                <h1 class="title"><?php dp_post_link(); ?></h1>
-                <div class="tags"><?php dp_post_tags('', '', ''); ?> by <?php dp_nick_name(); ?> at <?php dp_post_date(); ?></div>
+                <h1 class="title"><?php app_post_link(); ?></h1>
+                <div class="tags"><?php app_post_tags('', '', ''); ?> by <?php app_nick_name(); ?> at <?php app_post_date(); ?></div>
                 <div class="clearer"></div>
               </div>
-            <?php   } ?>
+            <?php } ?>
             <div id="page_bar">
-              <?php if (dp_has_new()) { ?>
-                <span class="prev link" style="float:left;"><?php dp_goto_new('&larr; 较新文章'); ?></span>
+              <?php if (app_has_new()) { ?>
+                <span class="prev link" style="float:left;"><?php app_goto_new('&larr; 较新文章'); ?></span>
               <?php } ?>
-              <?php if (dp_has_old()) { ?>
-                <span class="next link" style="float:right;"><?php dp_goto_old('早期文章 &rarr;'); ?></span>
+              <?php if (app_has_old()) { ?>
+                <span class="next link" style="float:right;"><?php app_goto_old('早期文章 &rarr;'); ?></span>
               <?php   } ?>
               <div class="clearer"></div>
             </div>
@@ -98,8 +97,8 @@ if (!isset($dp_config)) exit;
     <div class="clearer"></div>
     <div id="footer">
       <div>Powered by <a href="http://doupress.org" class="link" target="_blank">DouPress</a></div>
-      <?php if (!empty($dp_config['site_icpno'])) { ?><p><a href="https://beian.miit.gov.cn/#/Integrated/index" class="link" target="_blank"><?php echo $dp_config['site_icpno'];?></a></p><?php } ?>
-      <?php dp_footer_code(); ?>
+      <?php if (!empty($app_config['site_icpno'])) { ?><p><a href="https://beian.miit.gov.cn/#/Integrated/index" class="link" target="_blank"><?php echo $app_config['site_icpno'];?></a></p><?php } ?>
+      <?php app_footer_code(); ?>
     </div>
   </div>
 </body>
